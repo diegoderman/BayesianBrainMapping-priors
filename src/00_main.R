33f6455268468ecefed80b05aeed65d42e95c44d
Diego Derman <diegoderman@gmail.com>
2026-01-15 10:23:11 -0500

    Fixed FD flag list

--- src/00_main.R
+++ src/00_main.R
@@ -1,4 +1,8 @@
-# Main script to run demo stage
+# Main script to run reproducibility GitHub repository 
+
+# Set scriptdir as working directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 source("0_setup.R")
 
 # Run framewise displacement filtering 
--- src/0_setup.R
+++ src/0_setup.R
@@ -9,6 +9,7 @@
 # install.packages("fMRItools")          
 # install.packages("viridis")
 # install.packages("BayesBrainMap")
+# devtools::install_github("mandymejia/BayesBrainMap", ref = "2.0")
 
 # Load packages
 library(ggcorrplot)      # version 0.1.4.1
@@ -17,7 +18,7 @@ library(ciftiTools)      # version 0.17.4
 library(fMRIscrub)       # version 0.14.7
 library(fMRItools)       # version 0.5.3
 library(viridis)         # version 0.6.5
-library(BayesBrainMap)   # version: 0.1.1 
+library(BayesBrainMap)   # version: 0.2.0
 
 # Set CIFTI Workbench path
 wb_path <- "~/Downloads/workbench"
@@ -25,12 +26,12 @@ ciftiTools.setOption("wb_path", wb_path)
 
 # Set up paths
 dir_HCP <- "/N/project/hcp_dcwan" # Location of HCP data
-dir_project <- "~/Documents/GitHub/BayesianBrainMapping-Priors" # Path to GitHub folder
+dir_project <- "~/Documents/GitHub/BayesianBrainMapping-priors" # Path to GitHub folder
 
 dir_data <- file.path(dir_project, "data_OSF") # Path to data folder
 
 # Bring your own HCP access for both restricted and unrestricted data
-dir_HCP_demo <- "~/Documents/GitHub/BayesianBrainMapping-Priors/data_OSF/inputs/HCP_demo" # Path to folder with HCP demographics CSVs
+dir_HCP_demo <- "~/Documents/GitHub/BayesianBrainMapping-priors/data_OSF/inputs/HCP_demo" # Path to folder with HCP demographics CSVs
 
 # HCP_unrestricted_fname <- file.path(dir_data, "inputs", "unrestricted_HCP_demographics.csv")
 HCP_restricted_fname <- file.path(dir_HCP_demo, "restricted_HCP.csv")
