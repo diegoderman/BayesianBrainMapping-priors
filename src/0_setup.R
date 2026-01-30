f1d0730ac3feabdf212696aabacb0869d0006173
Diego Derman <diegoderman@gmail.com>
2026-01-30 15:34:37 -0500

    progress on single subject inference

--- src/0_setup.R
+++ src/0_setup.R
@@ -57,7 +57,7 @@ nT_HCP <- 1200 # Timepoints for each resting state scan
 min_total_sec <- 600 # Minimum duration of time series after scrubbing (600 sec = 10 min)
 
 # Calculation constants
-nThreads = 62 # number of threads to use to estimate priors
+nThreads = 40 # number of threads to use to estimate priors
 
 # Parameter sweep definition for prior estimation
 encoding_sweep = c("combined") # Using only combined c("LR", "RL", "combined") 
